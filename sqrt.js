97dd37d7e08d4a8472d8c617109f7b449ca61254
Nathanage3 <sisay954@gmail.com>
2023-11-04 01:56:50 +0300

    Evaluating thr sqaure root manually

--- sqrt.js
+++ sqrt.js
@@ -1,28 +1,29 @@
-let x = 15;
+let x = 17;
 let sqrt_found = false;
 let approx_guess = 0;
 
 if (x < 0) {
     console.log('Negative numbers don\'t have a square root in the real numbers.');
-}
-else {
-    for (let i = 0; i <= x; i++) {
-        if (i * i === x) {
-            console.log(`The square root of ${x} is ${i}`);
-            sqrt_found = true;
-            break;
-        }
-        else if (i * i > x){
-            // Take the average of `x / i` and `i - 1`
-            approx_guess = ((x / i) + (i - 1)) / 2;
-            console.log(`The square root of ${x} is approximately ${approx_guess}`);
-            sqrt_found = true;
-            break;
-        }  
+} else {
+    let i = 1; // Start from 1 to avoid dividing by 0
+    while (i * i <= x) {
+        i++;
     }
+    // At this point, i is the smallest integer greater than the square root of x
+    let lowerBound = i - 1; // This is the largest integer less than the square root of x
+    approx_guess = (lowerBound + x / lowerBound) / 2; // Averaging
 
-    if (!sqrt_found) {
-        // In case x is 1 or 0, handle output here
-        console.log(`The square root of ${x} is approximately ${Math.sqrt(x)}`);
-    }
+    // Now refine the guess using a simple iteration
+    let previous_guess;
+    do {
+        previous_guess = approx_guess;
+        approx_guess = (approx_guess + x / approx_guess) / 2;
+    } while (Math.abs(previous_guess - approx_guess) > 1e-10); // Until the result is stable within the tolerance
+
+    console.log(`The square root of ${x} is approximately ${approx_guess}`);
+    sqrt_found = true;
+}
+
+if (!sqrt_found) {
+    console.log(`The square root of ${x} cannot be determined.`);
 }
